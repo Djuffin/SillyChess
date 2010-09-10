9757a446a4a67a481122ad532861572cd3a711c6
Eugene Zemtsov <e.zemtsov@mirantis.ru>
2010-09-10 04:05:04 +0400

    attempts to declare show instances

--- chess.hs
+++ chess.hs
@@ -1,10 +1,17 @@
 module Chess where
 
 data Kind = King | Queen | Rook | Bishop | Knight | Pawn
-                 deriving (Eq, Show)
+                 deriving (Eq)
 				 
 data Color = White | Black
-                 deriving (Eq, Show)
+                 deriving (Eq)
 				 
 data Piece = Piece Color Kind
-				deriving (Eq, Show)
+				deriving (Eq)
+				
+data Square	= Square Int Int -- column, row (from zero)
+
+type Line = [Maybe Piece]				
+
+type Board = [Line] -- from 1 to 9
+
--- main.hs
+++ main.hs
@@ -1,9 +1,33 @@
+{-# LANGUAGE TypeSynonymInstances, OverlappingInstances #-}
+ 
 module Main (main) where
 
 import System.IO
+import Data.List
 
 import Chess
 
 main = do
-	putStrLn $ show $ Piece White Rook
+	putStrLn $ show $ [Just (Piece White Rook), Just (Piece Black Queen), Nothing] 
+	
+	
+-- show instances
+
+instance Show Kind where
+	show King = "K"
+	show Queen = "Q"
+	show Rook = "R"
+	show Bishop = "B"
+	show Knight = "N"
+	show Pawn = "P"
+	
+instance Show Piece where	
+	show (Piece color kind) = show kind
+	
+instance Show Line where
+	show line = concat . intersperse ", " . map showSquare line
+					where 
+						showSquare Nothing = " "
+						showSquare (Just p) = show p
+					
 	
