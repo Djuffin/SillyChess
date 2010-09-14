c4cd175d23dd4211883d72b22b8794234e7d1b23
Eugene Zemtsov <e.zemtsov@mirantis.ru>
2010-09-14 04:24:48 +0400

    minor improvements of FEN rendering

--- chess.hs
+++ chess.hs
@@ -57,14 +57,14 @@ renderFEN (Position (Board lines) nextToMove whiteCastling blackCastling enPassa
 			piecesStr ++ " " ++ (renderNextToMove nextToMove)++ " " ++ (renderCastlings whiteCastling blackCastling) ++ " " ++ (renderEnPass enPassant) ++ " " ++ halfMoveStr ++ " " ++ movesStr
 			where
 				piecesStr = intercalate "/" $ map renderLine $ reverse lines
-				renderLine (Line pieces) = foldEmptySqueres $ concat $ map renderPiece pieces
+				renderLine (Line pieces) = foldEmptySqueres $ map renderPiece pieces
 				foldEmptySqueres (d:'1':xs) | isDigit d = (foldEmptySqueres $ (succ d):xs)
 				foldEmptySqueres (x:xs) = x:(foldEmptySqueres xs)
 				foldEmptySqueres [] = []
-				renderPiece Nothing = "1"
+				renderPiece Nothing = '1'
 				renderPiece (Just (Piece White kind)) = renderKind kind
-				renderPiece (Just (Piece Black kind)) = map toLower $ renderKind kind
-				renderKind kind = fromJust $ lookup kind [(King, "K"), (Queen, "Q"), (Rook, "R"), (Bishop, "B"), (Knight, "N"), (Pawn, "P")] 
+				renderPiece (Just (Piece Black kind)) = toLower $ renderKind kind
+				renderKind kind = fromJust $ lookup kind [(King, 'K'), (Queen, 'Q'), (Rook, 'R'), (Bishop, 'B'), (Knight, 'N'), (Pawn, 'P')] 
 				renderNextToMove White = "w"
 				renderNextToMove Black = "b"
 				renderCastlings NoCastling NoCastling = "-"
@@ -75,6 +75,12 @@ renderFEN (Position (Board lines) nextToMove whiteCastling blackCastling enPassa
 				renderSquere (row, column) = (['a' .. 'h'] !! column) : (show $ 1 + column)
 				halfMoveStr = show halfMovesSinceAction
 				movesStr = show fullMoves
+				
+readFEN :: String -> Maybe Position
+readFEN fen = let parts = words fen
+				in Nothing
+				
+				
 
 				
 
