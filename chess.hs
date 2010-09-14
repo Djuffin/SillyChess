9b117c3134efc3d5936c18de0e78dc054c5e593e
Eugene Zemtsov <e.zemtsov@mirantis.ru>
2010-09-14 04:00:48 +0400

    export Position to FEN works

--- chess.hs
+++ chess.hs
@@ -1,5 +1,9 @@
 module Chess where
 
+import Data.List
+import Data.Char
+import Data.Maybe
+
 data Kind = King | Queen | Rook | Bishop | Knight | Pawn
                  deriving (Eq)
 				 
@@ -15,14 +19,17 @@ data Line = Line [Maybe Piece]
 
 data Board = Board [Line] -- rows from 1 to 8
 
-data Castling = None | QueenCastling | KingCastling | Both
+data Castling = NoCastling | QueenCastling | KingCastling | BothCastling
+				deriving (Eq)
 
 data Position = Position {
 					board :: Board,
 					nextToMove :: Color,
 					whiteCastling :: Castling,
 					blackCastling :: Castling,
-					enPassant :: Maybe Square					
+					enPassant :: Maybe Square,
+					halfMovesSinceAction :: Int,
+					fullMoves :: Int
 				}
 
 getPieceOfLine :: Line -> Int -> Maybe Piece
@@ -41,3 +48,33 @@ initialBoard = Board [
 					(Line [Just (Piece Black Pawn), Just (Piece Black Pawn), Just (Piece Black Pawn), Just (Piece Black Pawn), Just (Piece Black Pawn), Just (Piece Black Pawn), Just (Piece Black Pawn), Just (Piece Black Pawn)]),
 					(Line [Just (Piece Black Rook), Just (Piece Black Knight), Just (Piece Black Bishop), Just (Piece Black Queen), Just (Piece Black King), Just (Piece Black Bishop), Just (Piece Black Knight), Just (Piece Black Rook)])
 				]
+
+initialPosition = Position initialBoard White BothCastling BothCastling Nothing 0 1
+
+--Forsyth–Edwards Notation
+renderFEN :: Position -> String
+renderFEN (Position (Board lines) nextToMove whiteCastling blackCastling enPassant halfMovesSinceAction fullMoves) = 
+			piecesStr ++ " " ++ (renderNextToMove nextToMove)++ " " ++ (renderCastlings whiteCastling blackCastling) ++ " " ++ (renderEnPass enPassant) ++ " " ++ halfMoveStr ++ " " ++ movesStr
+			where
+				piecesStr = intercalate "/" $ map renderLine $ reverse lines
+				renderLine (Line pieces) = foldEmptySqueres $ concat $ map renderPiece pieces
+				foldEmptySqueres (d:'1':xs) | isDigit d = (foldEmptySqueres $ (succ d):xs)
+				foldEmptySqueres (x:xs) = x:(foldEmptySqueres xs)
+				foldEmptySqueres [] = []
+				renderPiece Nothing = "1"
+				renderPiece (Just (Piece White kind)) = renderKind kind
+				renderPiece (Just (Piece Black kind)) = map toLower $ renderKind kind
+				renderKind kind = fromJust $ lookup kind [(King, "K"), (Queen, "Q"), (Rook, "R"), (Bishop, "B"), (Knight, "N"), (Pawn, "P")] 
+				renderNextToMove White = "w"
+				renderNextToMove Black = "b"
+				renderCastlings NoCastling NoCastling = "-"
+				renderCastlings white black = (renderOneCastling white) ++ (map toLower $ renderOneCastling black)
+				renderOneCastling castling = fromJust $ lookup castling [(BothCastling ,"KQ"), (KingCastling, "K"), (QueenCastling, "Q"), (NoCastling, "")]
+				renderEnPass Nothing = "-" 
+				renderEnPass (Just sq) = renderSquere sq
+				renderSquere (row, column) = (['a' .. 'h'] !! column) : (show $ 1 + column)
+				halfMoveStr = show halfMovesSinceAction
+				movesStr = show fullMoves
+
+				
+
--- main.hs
+++ main.hs
@@ -5,22 +5,18 @@ module Main (main) where
 import System.IO
 import Data.List
 import Data.Char
+import Data.Maybe
 
 import Chess
 
 main = do
-	putStrLn $ show $ initialBoard	
+	putStrLn $ renderFEN $ initialPosition	
 	
 	
 -- show instances
 
 instance Show Kind where
-	show King = "K"
-	show Queen = "Q"
-	show Rook = "R"
-	show Bishop = "B"
-	show Knight = "N"
-	show Pawn = "P"
+	show kind = fromJust $ lookup kind [(King, "K"), (Queen, "Q"), (Rook, "R"), (Bishop, "B"), (Knight, "N"), (Pawn, "P")] 
 	
 instance Show Piece where	
 	show (Piece White kind) = show kind
