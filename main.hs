39b40f2a82e1fe64f80040700c9b4cb92cf2b28c
Eugene Zemtsov <e.zemtsov@mirantis.ru>
2010-09-23 03:44:41 +0400

    little demo

--- main.hs
+++ main.hs
@@ -6,14 +6,29 @@ import System.IO
 import Data.List
 import Data.Char
 import Data.Maybe
+import System.Random
 
 import Chess
 import FEN
 
 main = do
 	--putStrLn $ renderFEN $ initialPosition
-	let moves = getAllLegalMoves initialPosition
-	let strMoves = intersperse " " $ map show moves
-	putStrLn $ concat strMoves
+	step (mkStdGen 1) initialPosition
 
 
+step :: StdGen -> Position -> IO ()
+step g p = do
+	putStrLn "--------------------------------------------"
+	putStrLn $ show p
+	let moves = getAllLegalMoves p
+	let (index, newG) = random g 
+	let isEnd = (null moves) || (halfMovesSinceAction p > 50)
+	if isEnd then return () 
+				else do
+					let move = (moves !! (index `mod` (length moves)))
+					putStrLn $ show move 
+					step newG $ applyMove p move
+
+			
+								
+	
